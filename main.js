75ef032b819ecca4f80bd079054b6e0f240ca436
ChristianBaciu <christian3110@outlook.it>
2024-01-10 00:12:53 +0100

    fizzBuzz

--- main.js
+++ main.js
@@ -1,19 +1,50 @@
-// Scrivi un programma che stampi in console i numeri da 1 a 100, 
-// ma che per i multipli di 3 stampi “Fizz” al posto del numero e
-// per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia
-// multipli di 3 che di 5 stampi “FizzBuzz”.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per un altro? 
-// Abbiamo visto qualcosa di particolare che possiamo usare?
-// Consigli del giorno:
-// Scriviamo sempre prima dei commenti in italiano per capire cosa 
-// vogliamo fare
-// Proviamo ad immaginare le operazioni che vogliamo far svolgere
-// al nostro programma così come lo faremmo "a mano"
-// BONUS 1: Crea un container nel DOM , aggiungendo (attraverso la
-// funzione append()) un elemento html con il numero o la 
-// stringa corretta da mostrare.
-// BONUS 2: Applica stili differenti agli elementi aggiunti al 
-// DOM nel BONUS 1, a seconda che il valore inserito sia un numero,
-// un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo 
-// stile, potresti prendere spunto dallo screenshot fornito in consegna.
+const ul = document.querySelector( 'ul.list' )
+
+for( let i = 0; i <= 100; i++){
+
+    if(i % 15 == 0){
+        const element = '<li style="color: red" >fizzBuzz</li>'
+        ul.innerHTML += element
+
+    }else if(i % 3 == 0){
+        const element = '<li style="color: blue" >fizz</li>'
+        ul.innerHTML += element
+        
+    }else if(i % 5 == 0){
+        const element = '<li style="color: orange" >Buzz</li>'
+        ul.innerHTML += element
+
+    }else{
+        const element = '<li style="color: black" ></li>'
+        ul.innerHTML += element
+
+    }
+}
+
+
+// for( let x = 0; x < 50; x++){
+//     console.log(x)
+// }
+
+
+// const ul = document.querySelector( 'ul.list' )
+
+// for( let i = 0; i <= 100; i++){
+
+//     if( (i % 3) === 0 ){
+//         const element = `<li style="color: red" > ${i}frizz3 </li>`
+//         ul.innerHTML += element
+//     }else{
+//         const element = `<li style="color: blue" > ${i} </li>`
+//         ul.innerHTML += element
+//     }
+// }
+
+
+
+// const ul = document.querySelector( 'ul.list' )
+
+// for( let index = 0; index < 100; index++){
+//     const element = `<li  calss="box box--${index}"> Numero ${index} </li>`
+//     ul.innerHTML += element;
+// }
